d2c1eb683af1570cfa704fab6454963bd393bb54
Niukun <1464042197@qq.com>
2017-03-03 14:11:02 +0800

    生成大语料词向量

--- trunk/Word2Vec_1.0/src/main/java/niukun/LoadModel_02.java
+++ trunk/Word2Vec_1.0/src/main/java/niukun/LoadModel_02.java
@@ -1,6 +1,7 @@
 package niukun;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.deeplearning4j.models.embeddings.WeightLookupTable;
 import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
@@ -9,21 +10,20 @@ import org.deeplearning4j.models.word2vec.Word2Vec;
 public class LoadModel_02 {
 
 	public static void main(String[] args) {
-		Word2Vec word2Vec = WordVectorSerializer.readWord2VecModel("C:/D/NLPIR/paper/files/merge/noNormalize_Segment/vector.txt");
+//		Word2Vec word2Vec = WordVectorSerializer.readWord2VecModel("G:/学习工具/计算机/NLP/word2vec/bin/GoogleNews-vectors-negative300.bin.gz");
+		Word2Vec word2Vec = WordVectorSerializer.readWord2VecModel("C:/D/NLPIR/paper/files/merge/Normal/NormalVector.txt");
 		WeightLookupTable weightLookupTable = word2Vec.lookupTable();
 
-
-		Collection<String> lst = word2Vec.wordsNearest("男人", 10);
+		System.out.println(word2Vec.similarity("法国", "巴黎"));
+		System.out.println(word2Vec.similarity("美国", "纽约"));
+		System.out.println(word2Vec.similarity("男人", "女人"));
+		System.out.println(word2Vec.similarity("主席", "胡锦涛"));
+		System.out.println(word2Vec.similarity("枪支", "弹药"));
+		Collection<String> lst = word2Vec.wordsNearest("朱元璋", 10);
         System.out.println(lst);
-        lst = word2Vec.wordsNearest("乾隆", 10);
+        lst = word2Vec.wordsNearest("北京", 10);
         System.out.println(lst);
-        lst = word2Vec.wordsNearest("习近平", 10);
-        System.out.println(lst);
-        lst = word2Vec.wordsNearest("改革", 10);
-        System.out.println(lst);
-        lst = word2Vec.wordsNearest("枪", 10);
-        System.out.println(lst);
-        lst = word2Vec.wordsNearest("阅读", 10);
+        lst = word2Vec.wordsNearest("钱学森", 10);
         System.out.println(lst);
 		
 	}
