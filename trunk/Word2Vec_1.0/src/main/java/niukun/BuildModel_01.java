3dca5be30ef2ee58f142f35ab24496eb069e212b
Niukun <1464042197@qq.com>
2017-03-15 09:52:04 +0800

    固定这个训练使用模型
    以后直接拿来用就可以了

--- trunk/Word2Vec_1.0/src/main/java/niukun/BuildModel_01.java
+++ trunk/Word2Vec_1.0/src/main/java/niukun/BuildModel_01.java
@@ -1,6 +1,7 @@
 package niukun;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Collection;
 
 import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
@@ -14,11 +15,20 @@ import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.sun.xml.internal.messaging.saaj.soap.impl.ElementFactory;
+
 public class BuildModel_01 {
 	private static Logger log = LoggerFactory.getLogger(BuildModel_01.class);
 	public static void main(String[] args) throws Exception {
+//		getVector("C:/D/NLPIR/paper/files/merge/clean3.0/clean3.0.txt","C:/D/NLPIR/paper/files/merge/clean3.0/clean3.0_50_2.txt");
+//		getVector("E:/NLPIR/merge/tensite/tensiteSegstopWordsRemoved.txt","E:/NLPIR/merge/tensite/tensiteVector_50_2.txt");
+		getVector("E:/NLPIR/merge/sohusite_tensite/sohusite_tensiteSegstopWordsRemoved.txt","E:/NLPIR/merge/sohusite_tensite/sohusite_tensiteVector_50_2.txt");
+//		getVector("E:/NLPIR/merge/sohusite/sohusiteSegstopWordsRemoved.txt","E:/NLPIR/merge/sohusite/sohusiteVector_50_2.txt");
+        
+	}
+	public static void getVector(String srcFile,String vecFile) throws IOException {
 		log.info("Load data....");
-        SentenceIterator iter = new LineSentenceIterator(new File("C:/D/NLPIR/paper/files/merge/tensite/tensiteSegstopWordsRemoved.txt"));
+        SentenceIterator iter = new LineSentenceIterator(new File(srcFile));
         iter.setPreProcessor(new SentencePreProcessor() {
             public String preProcess(String sentence) {
                 return sentence.toLowerCase();
@@ -30,9 +40,9 @@ public class BuildModel_01 {
         
         log.info("Building model....");
         Word2Vec vec = new Word2Vec.Builder()
-                .minWordFrequency(20)
-                .iterations(1)
-                .layerSize(100)
+                .minWordFrequency(5)
+                .iterations(2)
+                .layerSize(50)
                 .seed(42)
                 .windowSize(5)
                 .iterate(iter)
@@ -42,14 +52,14 @@ public class BuildModel_01 {
         log.info("Fitting Word2Vec model....");
         vec.fit();
         
-        WordVectorSerializer.writeWordVectors(vec, "C:/D/NLPIR/paper/files/merge/tensite/tensiteVector.txt");
+        WordVectorSerializer.writeWordVectors(vec, vecFile);
 
-        log.info("Closest Words:");
-        Collection<String> lst = vec.wordsNearest("北京", 10);
-        System.out.println(lst);
+        log.info(srcFile + "Finished.....");
+        log.info("*********************************************************");
+//        Collection<String> lst = vec.wordsNearest("北京", 10);
+//        System.out.println(lst);
 //        UiServer server = UiServer.getInstance();
 //        System.out.println("Started on port " + server.getPort());
-        
 	}
 	
 }
--- trunk/Word2Vec_1.0/src/main/java/niukun/LoadModel_02.java
+++ trunk/Word2Vec_1.0/src/main/java/niukun/LoadModel_02.java
@@ -2,39 +2,27 @@ package niukun;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
-import org.deeplearning4j.models.embeddings.WeightLookupTable;
 import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
 import org.deeplearning4j.models.word2vec.Word2Vec;
 
 public class LoadModel_02 {
+	private static Word2Vec word2Vec;
+	private static Set<String> set = new TreeSet<String>();
+	private static List<String> list;
 
 	public static void main(String[] args) {
-//		Word2Vec word2Vec = WordVectorSerializer.readWord2VecModel("G:/学习工具/计算机/NLP/word2vec/bin/GoogleNews-vectors-negative300.bin.gz");
-		Word2Vec word2Vec = WordVectorSerializer.readWord2VecModel("C:/D/NLPIR/paper/files/merge/tensite/tensiteVector.txt");
-
-		System.out.println(word2Vec.similarity("法国", "巴黎"));
-		System.out.println(word2Vec.similarity("中国", "北京"));
-		System.out.println(word2Vec.similarity("男人", "女人"));
-		System.out.println(word2Vec.similarity("主席", "胡锦涛"));
-		System.out.println(word2Vec.similarity("孙悟空", "西游记"));
-		System.out.println(word2Vec.similarity("金庸", "小说"));
-		System.out.println(word2Vec.similarity("马云", "赵薇"));
-		System.out.println(word2Vec.similarity("枪支", "弹药"));
-		Collection<String> lst = word2Vec.wordsNearest("朱元璋", 10);
-        System.out.println(lst);
-        lst = word2Vec.wordsNearest("北京", 10);
-        System.out.println(lst);
-        lst = word2Vec.wordsNearest("中国", 10);
-        System.out.println(lst);
-        lst = word2Vec.wordsNearest("深圳", 10);
-        System.out.println(lst);
-        lst = word2Vec.wordsNearest("马云", 10);
-        System.out.println(lst);
-        lst = word2Vec.wordsNearest("历史", 10);
-        System.out.println(lst);
-        lst = word2Vec.wordsNearest("西兰花", 10);
-        System.out.println(lst);
+		 Word2Vec word2Vec = WordVectorSerializer.readWord2VecModel("G:/学习工具/计算机/NLP/word2vec/bin/GoogleNews-vectors-negative300.bin.gz");
+		 Collection<String> lst = word2Vec.wordsNearest("day", 10);
+		 for (String string : lst) {
+			System.out.println(string + "\t" + word2Vec.similarity(string, "day"));
+		 }
+//		word2Vec = WordVectorSerializer
+//				.readWord2VecModel("C:/D/NLPIR/paper/files/merge/sohusite_tensite/sohusite_tensiteVector_100_1.txt");
+		 
+//		 System.out.println(word2Vec.similarity("枪支", "弹药"));
 		
 	}
 
